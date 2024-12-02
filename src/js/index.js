4d7ced92c155277d5871144b81f06a40a59c976e
uiuxkimabe <uiuxkimabe@gmail.com>
2024-12-02 22:45:13 +0700

    add navbar animation scroll

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
+// hamBtn Menu
 const menu = document.querySelector('.menu')
-const menuBtn = document.querySelector('.menuBtn').addEventListener('click',()=> menu.classList.toggle('show'))
+const menuBtn = document.querySelector('.menuBtn')
+const menuLink = document.querySelectorAll('.menu a')
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('show')
+})
+
+// Scroll Navbar
+const navbar = document.getElementsByTagName('nav')[0]
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        navbar.classList.add('show')
+    } else {
+        navbar.classList.remove('show')
+    }
+})
